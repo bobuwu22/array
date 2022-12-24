91b285bfbcad4fdbad039882aeda97bd13a7dc07
bob <bobuwu22@outlook.com>
2022-12-23 20:23:11 -0500

    Version 3.3

--- n.sh
+++ n.sh
@@ -4,6 +4,10 @@ len=$(jq "[.array[] | select(.)] | length" $indices)
 ocurrences=$(jq "[.array[] | select(. == $index)] | length" $indices)
 
 
+quotes="/home/bob/bin/array/qt.json"
+quoteLocal=$(jq ".[$index].text" $quotes)
+authorLocal=$(jq -r ".[$index].author" $quotes)
+
 
 if [ $len -lt 6 ]
 then
@@ -11,14 +15,18 @@ then
         do
                 index=$(shuf -i 0-5 -n 1)
 		ocurrences=$(jq "[.array[] | select(. == $index)] | length" $indices)
+		quoteLocal=$(jq ".[$index].text" $quotes)
+		authorLocal=$(jq -r ".[$index].author" $quotes)
         done
-
+	
+	echo -e "$index : $quoteLocal \n -$authorLocal" | lolcat
         newindices=$(jq ".array += [$index]" $indices)
 	echo $newindices > $indices
 fi
 
 if [ $len -eq 6 ]
 then 
+ 	echo -e "$index : $quoteLocal \n -$authorLocal" | lolcat	
 	newindices="{ \"array\": [] }"
 	echo $newindices > $indices
 fi
