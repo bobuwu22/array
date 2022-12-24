2c411ea96a83ebe906d1424765705d6833548434
bob <bobuwu22@outlook.com>
2022-12-23 20:25:22 -0500

    Version 3.4

--- dailyQuoteK.sh
+++
@@ -1,7 +0,0 @@
-index=$(shuf -i 0-5 -n 1)
-quotes="/home/bob/bin/array/qt.json"
-quoteLocal=$(jq ".[$index].text" $quotes)
-authorLocal=$(jq -r ".[$index].author" $quotes)
-
-
-echo -e "$index : $quoteLocal \n -$authorLocal" | lolcat
